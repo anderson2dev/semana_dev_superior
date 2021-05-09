70ea5d7350024054ad5477b7693275cd357250e4
anderson <subscriberandersom@gmail.com>
2021-05-09 01:06:36 -0300

    cleaning project

--- projeto_sds/frontend/src/App.tsx
+++ projeto_sds/frontend/src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Fragment } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h2> learning react</h2>
+      <div className="App">
+      </div>
+    </Fragment>
   );
 }
 
